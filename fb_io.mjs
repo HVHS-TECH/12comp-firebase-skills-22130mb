354f3f70fcd1df5524be638fadc9ad25c0c84b68
22130mb <22130mb@hvhs.school.nz>
2025-05-05 00:27:13 +0000

    .

--- fb_io.mjs
+++ fb_io.mjs
@@ -4,6 +4,31 @@
 // Written by <Your Name Here>, Term 2 202?
 //
 // All variables & function begin with fb_  all const with FB_
+
+function fb_initialise() {
+    console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    console.log ("hello world");
+    const FB_GAMECONFIG = {
+        apiKey: "AIzaSyCHDtQ5nuCxgp_XCL_RtR7YVHv8mO1rhmc",
+        authDomain: "comp-2025-max-bergman-4bb13.firebaseapp.com",
+        databaseURL: "https://comp-2025-max-bergman-4bb13-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "comp-2025-max-bergman-4bb13",
+        storageBucket: "comp-2025-max-bergman-4bb13.firebasestorage.app",
+        messagingSenderId: "75891205088",
+        appId: "1:75891205088:web:9ce6dd10fe8f59fb6f8185",
+        measurementId: "G-860HVWZ49V"
+      };
+     
+      const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);
+    
+      const FB_GAMEDB  = getDatabase(FB_GAMEAPP);
+      
+      
+      console.info(FB_GAMEDB);   
+
+      
+}
+
 // Diagnostic code lines have a comment appended to them //DIAG
 /**************************************************************/
 const COL_C = 'white';	    // These two const are part of the coloured 	
@@ -15,25 +40,19 @@ console.log('%c fb_io.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
-import { initializeApp }
-
-    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-
-import { getDatabase }
-
-    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 /**************************************************************/
 // EXPORT FUNCTIONS
-// List all the functions called by code or html outside of this module
-/**************************************************************/
-export { 
-    fb_initialise };
 
-function fb_initialise() {
-    console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
-    console.log ("hello world");
-}
+// List all the functions called by code or html outside of this module
+
+/**************************************************************/
+export { fb_initialise };
+
+
+
 
 /**************************************************************/
 // END OF CODE
